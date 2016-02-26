d42438585cb37082a606358619c550d1807fcb0a
Jesse Glick <jglick@cloudbees.com>
2016-02-26 15:59:38 -0500

    Linking to https://github.com/jenkinsci/jenkins/pull/2066.

--- src/main/java/org/jenkinsci/plugins/credentialsbinding/impl/FileBinding.java
+++ src/main/java/org/jenkinsci/plugins/credentialsbinding/impl/FileBinding.java
@@ -88,7 +88,7 @@ public class FileBinding extends Binding<FileCredentials> {
         return tempDir(workspace).child("secretFiles");
     }
 
-    // TODO move to WorkspaceList
+    // TODO https://github.com/jenkinsci/jenkins/pull/2066
     private static FilePath tempDir(FilePath ws) {
         return ws.sibling(ws.getName() + System.getProperty(WorkspaceList.class.getName(), "@") + "tmp");
     }

fa4ff53f1dd67edd1de265f3801bbd95cfb5c8b5
Matt Sicker <boards@gmail.com>
2019-05-03 11:43:33 -0500

    Improve javadoc
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- src/main/java/org/jenkinsci/plugins/credentialsbinding/masking/PatternMaskerProvider.java
+++ src/main/java/org/jenkinsci/plugins/credentialsbinding/masking/PatternMaskerProvider.java
@@ -44,12 +44,24 @@ import java.util.regex.Pattern;
  */
 public interface PatternMaskerProvider extends ExtensionPoint {
 
+    /**
+     * Returns a collection of alternative forms as regular expressions the given input may show up as in logs. These
+     * patterns should be appropriately {@linkplain Pattern#quote(String) quoted} for matching literal patterns as
+     * the resulting strings are formed into a regular expression.
+     */
     @Nonnull Collection<String> getAlternativeForms(@Nonnull String input);
 
+    /**
+     * Returns all PatternMaskerProvider extensions known at runtime.
+     */
     static @Nonnull ExtensionList<PatternMaskerProvider> all() {
         return ExtensionList.lookup(PatternMaskerProvider.class);
     }
 
+    /**
+     * Returns a collection of all alternative form regular expressions for the given input using all PatternMaskerProvider
+     * extensions.
+     */
     @Restricted(NoExternalUse.class)
     static @Nonnull Collection<String> getAllAlternateForms(@Nonnull String input) {
         Collection<String> alternateForms = new HashSet<>();
@@ -59,6 +71,15 @@ public interface PatternMaskerProvider extends ExtensionPoint {
         return alternateForms;
     }
 
+    /**
+     * Constructs a regular expression to match against all known forms that the given collection of input strings may
+     * appear. This pattern is optimized such that longer masks are checked before shorter masks. By doing so, this
+     * makes inputs that are substrings of other inputs be masked as the longer input, though there is no guarantee
+     * this will work in every possible situation. Another consequence of this ordering is that inputs that require
+     * quoting will be masked before a substring of the input was matched, thus avoiding leakage of information.
+     * For example, {@code bash -x} will only quote arguments echoed when necessary. To avoid leaking the presence or
+     * absence of quoting, the longer form is masked.
+     */
     @Restricted(NoExternalUse.class)
     static @Nonnull Pattern getMaskingPattern(@Nonnull Collection<String> inputs) {
         Collection<String> patterns = new TreeSet<>(Comparator.comparingInt(String::length).reversed().thenComparing(String::compareTo));
